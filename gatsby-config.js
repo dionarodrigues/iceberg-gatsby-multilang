8fe3c5a5c132a2d288a4be01127559281f616795
diogorodrigues <diogo.rodrigues.diogo@gmail.com>
2019-10-18 11:53:55 -0300

    Refactoring

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Gatsby multiple language starter`,
     description: `A simple but feature rich starter boilerplate for creating a multi-language Gatsby website (Internationalization / i18n) without third party plugins or packages and also focused on SEO. This starter also contains other main Gatsby configurations you might need.`,
     author: `@diogorodrigues`,
-    siteUrl: `http://www.diogorodrigues.com.br`,
+    siteUrl: `https://iceberg-gatsby-multilang.netlify.com/`,
   },
   plugins: [
     `gatsby-plugin-netlify-cms`,
