caed7d0d2656c0172ff38983dbee9c29034cbfd4
diogorodrigues <diogo.rodrigues.diogo@gmail.com>
2020-07-11 18:50:11 -0300

    Refactoring Layout Component

--- src/layouts/BaseLayout.js
+++ src/layouts/BaseLayout.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import Header from '../Header';
-import Footer from '../Footer';
-import GlobalStyles from '../../styles/global';
+import GlobalStyles from '../styles/global';
 
-import { useLocale } from '../../hooks/locale';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+import { useLocale } from '../hooks/locale';
 
 import * as S from './styled';
 
--- wrapPageElement.js
+++ wrapPageElement.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import AppProvider from './src/hooks';
-import { Layout } from './src/components/Layout';
+import { BaseLayout } from './src/layouts/BaseLayout';
 
 // Pass all props (hence the ...props) to the layout component so it has access to things like pageContext or location
 const wrapPageElement = ({ element, props }) => (
   <AppProvider>
-    <Layout {...props}>{element}</Layout>
+    <BaseLayout {...props}>{element}</BaseLayout>
   </AppProvider>
 );
 
